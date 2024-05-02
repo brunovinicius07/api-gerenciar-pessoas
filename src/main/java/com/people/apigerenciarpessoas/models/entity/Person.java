772be60bebfa86ff49ca99a5f797857ec7ccd2e7
Bruno Vinicius <brunoviniciusoliveiratavares@gmail.com>
2024-05-02 18:46:30 -0300

    person entity created

---
+++ src/main/java/com/people/apigerenciarpessoas/models/entity/Person.java
@@ -0,0 +1,47 @@
+package com.people.apigerenciarpessoas.models.entity;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Past;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.io.Serial;
+import java.io.Serializable;
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity(name = "tb_person")
+public class Person implements Serializable {
+
+    @Serial
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long idPeople;
+
+    @Size(max = 100)
+    @NotBlank
+    private String name;
+
+    @Past
+    @NotNull
+    private LocalDate birthDate;
+
+    @Size(max = 11)
+    @NotNull
+    private String cpf;
+
+    @OneToMany(mappedBy = "person")
+    private List<Address> addresses = new ArrayList<>();
+}
