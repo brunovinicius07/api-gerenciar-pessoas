366180da60bbe8d8d4f556758d3aaf68e13150ac
Bruno Vinicius <brunoviniciusoliveiratavares@gmail.com>
2024-05-02 18:40:49 -0300

    project started

---
+++ src/main/java/com/people/apigerenciarpessoas/ApiGerenciarPessoasApplication.java
@@ -0,0 +1,13 @@
+package com.people.apigerenciarpessoas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiGerenciarPessoasApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApiGerenciarPessoasApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/people/apigerenciarpessoas/ApiGerenciarPessoasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.people.apigerenciarpessoas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiGerenciarPessoasApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
